b96b3d75871c4f1cac92fb64c2318abbb0ff7260
Ahsan5313 <81156921+Ahsan5313@users.noreply.github.com>
2021-04-04 09:26:50 +0600

    add html and javascript

---
+++ index.js
@@ -0,0 +1,6 @@
+var text="ahsanul";
+var name=" huq<br/>";
+var address="brahmanbaria college para ";
+var ful="i have been  living for two years at college par  ";
+document.write(text+name+address+ful+ "couse you know that we get lot of ficility here as a s brahmanbaria cityzen ");
+
